166b3c47a206fb965bed68699ca10a17210ac00d
uksinha-sudo <sinhanikhil377@gmail.com>
2026-07-04 21:24:04 +0530

    Added CORS Policy settings in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
 
 
 app.use("/api/v1/user", userRouter);
