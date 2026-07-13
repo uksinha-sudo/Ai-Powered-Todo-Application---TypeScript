3fe1890ea255d1a8984e9b6a3797258bb66fef7f
uksinha-sudo <sinhanikhil377@gmail.com>
2026-07-13 18:12:00 +0530

    Fixed missing api

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const BACKEND_URL_USER = `${BASE_URL}/v1/user`;
-export const BACKEND_URL_TASK = `${BASE_URL}/v1/todo`;
+export const BACKEND_URL_USER = `${BASE_URL}/api/v1/user`;
+export const BACKEND_URL_TASK = `${BASE_URL}/api/v1/todo`;
